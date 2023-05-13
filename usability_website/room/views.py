a1d47b013a90bf0cb8481cab121f68cf2aca061c
David <dlla8930@uni.sydney.edu.au>
2023-05-13 23:37:14 +1000

    first

---
+++ usability_website/room/views.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+from .models import Message, Room
+# Create your views here.
+
+@login_required
+def rooms(request):
+    rooms = Room.objects.all()
+
+    return render(request, 'room/rooms.html', {'rooms': rooms})
+
+@login_required
+def room(request, slug):
+    room = Room.objects.get(slug=slug)
+
+    messages = Message.objects.filter(room=room).order_by('-date_added')[0:25][::-1]
+
+    return render(request, 'room/room.html', {'room': room, 'messages': messages})
